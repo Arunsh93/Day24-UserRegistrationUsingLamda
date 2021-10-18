bbfb3f9fa80cbe90b6760150ba7538615b7c04c6
Arunkumar Hosur <87358208+Arunsh93@users.noreply.github.com>
2021-10-18 18:04:16 +0530

    Add files via upload

---
+++ UserRegistrationDay24/UserRegisrationCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationDay24
+{
+    class UserRegisrationCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            Invalid_FirstName,
+            Invalid_LastName,
+            Invalid_EmailId,
+            Invalid_Password,
+            Invalid_PhoneNumber,
+        }
+        private ExceptionType type;
+        public UserRegisrationCustomException(ExceptionType type,string message):base(message)
+        {
+            this.type = type;
+        }
+    }
+}
