88edd5c27473df8803e3522c122169920807ffc5
Arunsh93 <Arunhosur93@gmail.com>
2021-10-18 16:12:10 +0530

    JUnit test for user registartion

--- UserRegistrationUsingLamda/Patterns.cs
+++ UserRegistrationUsingLamda/Patterns.cs
@@ -7,10 +7,79 @@ namespace UserRegistrationUsingLamda
 {
     public class Patterns
     {
-        public Func<string, string, bool> ValidPasswordRule = (x, y) => Regex.IsMatch(x, y);
-        public Func<string, string, bool> ValidFirstName = (x, y) => Regex.IsMatch(x, y);
-        public Func<string, string, bool> ValidLastName = (x, y) => Regex.IsMatch(x, y);
-        public Func<string, string, bool> ValidEmailId = (x, y) => Regex.IsMatch(x, y);
-        public Func<string, string, bool> ValidPhoneNumber = (x, y) => Regex.IsMatch(x, y);
+        public static string RegexPassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#$&])[A-Za-z0-9.@!#$&]{8,}$";
+        public static string RegexName = "^[A-Z]{1}[a-z]{2,}$";
+        public static string RegexEmailId = "^[A-Za-z0-9]+([-.+][0-9]+)?[0-9]*[@][A-Za-z0-9]+[.][a-zA-Z]{2,}([.][a-z]{2,})?$";
+        public static string RegexPhoneNumber = "^[0-9]{2}[ ][0-9]{10}$";
+
+        public bool IsValidFirstName(string firstName)
+        {
+            bool flag;
+            do
+            {
+                flag = (Regex.IsMatch(firstName, RegexName));
+                if (flag == true)
+                    return true;
+                else
+                    return false;
+            }
+            while (flag == false);
+        }
+
+        public bool IsValidLastName(string lastName)
+        {
+            bool flag;
+            do
+            {
+                flag = (Regex.IsMatch(lastName, RegexName));
+                if (flag == true)
+                    return true;
+                else
+                    return false;
+            }
+            while (flag == false);
+        }
+
+        public bool IsValidEmailId(string emailId)
+        {
+            bool flag;
+            do
+            {
+                flag = (Regex.IsMatch(emailId, RegexEmailId));
+                if (flag == true)
+                    return true;
+                else
+                    return false;
+            }
+            while (flag == false);
+        }
+
+        public bool IsValidPassword(string password)
+        {
+            bool flag;
+            do
+            {
+                flag = (Regex.IsMatch(password, RegexPassword));
+                if (flag == true)
+                    return true;
+                else
+                    return false;
+            }
+            while (flag == false);
+        }
+
+        public bool IsValidPhoneNumber(string phoneNumber)
+        {
+            bool flag;
+            do
+            {
+                flag = (Regex.IsMatch(phoneNumber, RegexName));
+                if (flag == true)
+                    return true;
+                else
+                    return false;
+            }
+            while (flag == false);
+        }
     }
 }
