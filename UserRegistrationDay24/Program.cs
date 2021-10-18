1b26cb317d407b71c974a26acb252fbd93ea468a
Arunsh93 <Arunhosur93@gmail.com>
2021-10-18 17:22:31 +0530

    Validate Sample EmailIds Using Junit

---
+++ UserRegistrationDay24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationDay24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+        }
+    }
+}
